9858cc3cc7f62580723aec198d03f42505e6f608
nxkhxl-04 <jagadeeshnikhil57@gmail.com>
2026-08-23 12:35:12 +1000

    Format pause menu UI

--- source/core/src/main/com/csse3200/game/pausemenu/PauseMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/pausemenu/PauseMenuDisplay.java
@@ -6,49 +6,46 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.csse3200.game.ui.UIComponent;
 
-
 public class PauseMenuDisplay extends UIComponent {
 
-    private Table table;
+  private Table table;
 
-    @Override
-    public void create() {
-        super.create();
-        addActors();
-    }
+  @Override
+  public void create() {
+    super.create();
+    addActors();
+  }
 
-    private void addActors() {
-        table = new Table();
-        table.setFillParent(true);
+  private void addActors() {
+    table = new Table();
+    table.setFillParent(true);
 
-        Label title = new Label("PAUSED", skin);
+    Label title = new Label("PAUSED", skin);
 
-        TextButton resumeBtn = new TextButton("Resume", skin);
-        TextButton restartBtn = new TextButton("Restart", skin);
-        TextButton mainMenuBtn = new TextButton("Main Menu", skin);
+    TextButton resumeBtn = new TextButton("Resume", skin);
+    TextButton restartBtn = new TextButton("Restart", skin);
+    TextButton mainMenuBtn = new TextButton("Main Menu", skin);
 
-        table.add(title);
-        table.row();
+    table.add(title);
+    table.row();
 
-        table.add(resumeBtn).padTop(20f);
-        table.row();
+    table.add(resumeBtn).padTop(20f);
+    table.row();
 
-        table.add(restartBtn).padTop(15f);
-        table.row();
+    table.add(restartBtn).padTop(15f);
+    table.row();
 
-        table.add(mainMenuBtn).padTop(15f);
+    table.add(mainMenuBtn).padTop(15f);
 
-        stage.addActor(table);
-    }
+    stage.addActor(table);
+  }
 
-    @Override
-    public void draw(SpriteBatch batch) {
+  @Override
+  public void draw(SpriteBatch batch) {}
 
-    }
-
-    @Override
-    public void dispose() {
-        table.clear();
-        super.dispose();
-    }
+  @Override
+  public void dispose() {
+    table.clear();
+    super.dispose();
+  }
 }
