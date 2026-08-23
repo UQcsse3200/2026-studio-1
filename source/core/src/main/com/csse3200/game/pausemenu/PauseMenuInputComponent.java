a65e1e9df743fecd4668f7cc638139666c3743f6
aadityapuri06 <aadityapuri06@gmail.com>
2026-08-23 11:10:50 +1000

    Add PauseMenuInputComponent: detect navigation input

---
+++ source/core/src/main/com/csse3200/game/pausemenu/PauseMenuInputComponent.java
@@ -0,0 +1,47 @@
+package com.csse3200.game.pausemenu;
+
+import com.badlogic.gdx.Input;
+import com.csse3200.game.input.InputComponent;
+
+public class PauseMenuInputComponent extends InputComponent {
+    private PauseMenuComponent pauseMenu;
+
+    public PauseMenuInputComponent(){
+        super(10);
+    }
+
+    public PauseMenuInputComponent(PauseMenuComponent pauseMenu){
+        this();
+        this.pauseMenu = pauseMenu;
+    }
+
+    @Override
+    public void create(){
+        super.create();
+        pauseMenu = entity.getComponent(PauseMenuComponent.class);
+    }
+
+    @Override
+    public boolean keyDown(int keycode){
+        if (pauseMenu == null || !pauseMenu.isPaused()){
+            return false;
+        }
+
+        if (keycode == Input.Keys.UP){
+            entity.getEvents().trigger("navigateUp");
+            return true;
+        }
+
+        if (keycode == Input.Keys.DOWN){
+            entity.getEvents().trigger("navigateDown");
+            return true;
+        }
+
+        if (keycode == Input.Keys.ENTER || keycode == Input.Keys.SPACE){
+            entity.getEvents().trigger("confirmSelection");
+            return true;
+        }
+
+        return false;
+    }
+}
