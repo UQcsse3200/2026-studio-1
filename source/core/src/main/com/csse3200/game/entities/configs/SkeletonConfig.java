9a83b83c6f261b5bbfedd589baca5433e563ee56
PadawanTano733 <sarah.preston@student.uq.edu.au>
2026-08-26 09:45:49 +1000

    Updated the Entity Configs to include SkeletonConfig.java, an added Skeleton JSON object to the NPCs json file and added MeleeConfig to the attacks section within the entities/configs package. Feature -> Enemy Foundation; Task -> Melee Attacks

---
+++ source/core/src/main/com/csse3200/game/entities/configs/SkeletonConfig.java
@@ -0,0 +1,7 @@
+package com.csse3200.game.entities.configs;
+
+import com.csse3200.game.entities.configs.attacks.MeleeAttackConfig;
+
+public class SkeletonConfig extends BaseEntityConfig{
+    public MeleeAttackConfig melee;
+}
---
+++ source/core/src/main/com/csse3200/game/entities/configs/attacks/MeleeAttackConfig.java
@@ -0,0 +1,11 @@
+package com.csse3200.game.entities.configs.attacks;
+
+/**
+ * Configuration for a melee attack capability.
+ * Attached to any entity config that includes melee attacks.
+ */
+public class MeleeAttackConfig {
+    public int damage;
+    public float range;
+    public float cooldown;
+}
