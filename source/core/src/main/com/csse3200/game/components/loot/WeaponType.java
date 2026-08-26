4cfcbaff4b38e11a4cff3c741de73be9ea7eb2d2
Ruhan Mahajan <ruhan.mahajan@gmail.com>
2026-08-26 17:04:48 +1000

    Fix weapon item formatting

--- source/core/src/main/com/csse3200/game/components/loot/WeaponType.java
+++ source/core/src/main/com/csse3200/game/components/loot/WeaponType.java
@@ -1,7 +1,8 @@
 package com.csse3200.game.components.loot;
 
-/** Represents the different types of weapons available in the game. */
+// Represents the different types of weapons available in the game.
+
 public enum WeaponType {
-    SWORD,
-    BOW
+  SWORD,
+  BOW
 }
--- source/core/src/test/com/csse3200/game/components/loot/WeaponGeneratorTest.java
+++ source/core/src/test/com/csse3200/game/components/loot/WeaponGeneratorTest.java
@@ -7,55 +7,52 @@ import org.junit.jupiter.api.Test;
 
 class WeaponGeneratorTest {
 
-    @Test
-    void shouldGenerateSword() {
-        WeaponGenerator generator = new WeaponGenerator();
+  @Test
+  void shouldGenerateSword() {
+    WeaponGenerator generator = new WeaponGenerator();
 
-        WeaponItem weapon = generator.generateWeapon(WeaponType.SWORD, 1);
+    WeaponItem weapon = generator.generateWeapon(WeaponType.SWORD, 1);
 
-        assertEquals("Basic Sword", weapon.getName());
-        assertEquals(ItemType.WEAPON, weapon.getItemType());
-        assertEquals(WeaponType.SWORD, weapon.getWeaponType());
-        assertEquals(10, weapon.getDamage());
-    }
+    assertEquals("Basic Sword", weapon.getName());
+    assertEquals(ItemType.WEAPON, weapon.getItemType());
+    assertEquals(WeaponType.SWORD, weapon.getWeaponType());
+    assertEquals(10, weapon.getDamage());
+  }
 
-    @Test
-    void shouldGenerateBow() {
-        WeaponGenerator generator = new WeaponGenerator();
+  @Test
+  void shouldGenerateBow() {
+    WeaponGenerator generator = new WeaponGenerator();
 
-        WeaponItem weapon = generator.generateWeapon(WeaponType.BOW, 1);
+    WeaponItem weapon = generator.generateWeapon(WeaponType.BOW, 1);
 
-        assertEquals("Basic Bow", weapon.getName());
-        assertEquals(WeaponType.BOW, weapon.getWeaponType());
-        assertEquals(7, weapon.getDamage());
-    }
+    assertEquals("Basic Bow", weapon.getName());
+    assertEquals(WeaponType.BOW, weapon.getWeaponType());
+    assertEquals(7, weapon.getDamage());
+  }
 
-    @Test
-    void shouldIncreaseDamageForHigherTier() {
-        WeaponGenerator generator = new WeaponGenerator();
+  @Test
+  void shouldIncreaseDamageForHigherTier() {
+    WeaponGenerator generator = new WeaponGenerator();
 
-        WeaponItem tierOne = generator.generateWeapon(WeaponType.SWORD, 1);
-        WeaponItem tierTwo = generator.generateWeapon(WeaponType.SWORD, 2);
+    WeaponItem tierOne = generator.generateWeapon(WeaponType.SWORD, 1);
+    WeaponItem tierTwo = generator.generateWeapon(WeaponType.SWORD, 2);
 
-        assertEquals(10, tierOne.getDamage());
-        assertEquals(20, tierTwo.getDamage());
-    }
+    assertEquals(10, tierOne.getDamage());
+    assertEquals(20, tierTwo.getDamage());
+  }
 
-    @Test
-    void shouldRejectInvalidTier() {
-        WeaponGenerator generator = new WeaponGenerator();
+  @Test
+  void shouldRejectInvalidTier() {
+    WeaponGenerator generator = new WeaponGenerator();
 
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> generator.generateWeapon(WeaponType.SWORD, 0));
-    }
+    assertThrows(
+        IllegalArgumentException.class, () -> generator.generateWeapon(WeaponType.SWORD, 0));
+  }
 
-    @Test
-    void shouldRejectNullWeaponType() {
-        WeaponGenerator generator = new WeaponGenerator();
+  @Test
+  void shouldRejectNullWeaponType() {
+    WeaponGenerator generator = new WeaponGenerator();
 
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> generator.generateWeapon(null, 1));
-    }
+    assertThrows(IllegalArgumentException.class, () -> generator.generateWeapon(null, 1));
+  }
 }
