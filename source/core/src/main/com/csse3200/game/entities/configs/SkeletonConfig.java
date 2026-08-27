d0a801a6007395e5294a72e2ffb8962cfcd5a974
PadawanTano733 <sarah.preston@student.uq.edu.au>
2026-08-27 20:50:45 +1000

    Skeleton Config updated.

--- source/core/src/main/com/csse3200/game/entities/configs/SkeletonConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/SkeletonConfig.java
@@ -2,6 +2,9 @@ package com.csse3200.game.entities.configs;
 
 import com.csse3200.game.entities.configs.attacks.MeleeAttackConfig;
 
+/**
+ * Configuration for a skeleton basic enemy.
+ */
 public class SkeletonConfig extends BaseEntityConfig{
     public MeleeAttackConfig melee;
 }
