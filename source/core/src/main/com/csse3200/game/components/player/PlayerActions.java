8dc0586cf86b5a7e192d7f00cc72450913a0a885
Parth Gupta <Parth562006@gmail.com>
2026-08-31 21:59:04 +1000

    Add player attack damage

--- source/core/src/main/com/csse3200/game/components/player/PlayerActions.java
+++ source/core/src/main/com/csse3200/game/components/player/PlayerActions.java
@@ -3,27 +3,47 @@ package com.csse3200.game.components.player;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.components.Component;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.physics.BodyUserData;
+import com.csse3200.game.physics.PhysicsLayer;
+import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.services.ServiceLocator;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Action component for interacting with the player. Player events should be initialised in create()
  * and when triggered should call methods within this class.
  */
 public class PlayerActions extends Component {
-  private static final Vector2 MAX_SPEED = new Vector2(3f, 3f); // Metres per second
+  private static final Vector2 MAX_SPEED = new Vector2(3f, 3f);
 
   private PhysicsComponent physicsComponent;
+  private CombatStatsComponent combatStats;
+  private HitboxComponent hitboxComponent;
+
   private Vector2 walkDirection = Vector2.Zero.cpy();
   private boolean moving = false;
+
+  private final Set<Entity> enemiesInRange = new HashSet<>();
 
   @Override
   public void create() {
     physicsComponent = entity.getComponent(PhysicsComponent.class);
+    combatStats = entity.getComponent(CombatStatsComponent.class);
+    hitboxComponent = entity.getComponent(HitboxComponent.class);
+
     entity.getEvents().addListener("walk", this::walk);
     entity.getEvents().addListener("walkStop", this::stopWalking);
     entity.getEvents().addListener("attack", this::attack);
+
+    entity.getEvents().addListener("collisionStart", this::onCollisionStart);
+    entity.getEvents().addListener("collisionEnd", this::onCollisionEnd);
   }
 
   @Override
@@ -37,7 +57,6 @@ public class PlayerActions extends Component {
     Body body = physicsComponent.getBody();
     Vector2 velocity = body.getLinearVelocity();
     Vector2 desiredVelocity = walkDirection.cpy().scl(MAX_SPEED);
-    // impulse = (desiredVel - currentVel) * mass
     Vector2 impulse = desiredVelocity.sub(velocity).scl(body.getMass());
     body.applyLinearImpulse(impulse, body.getWorldCenter(), true);
   }
@@ -64,5 +83,50 @@ public class PlayerActions extends Component {
     Sound attackSound =
         ServiceLocator.getResourceService().getAsset("sounds/Impact4.ogg", Sound.class);
     attackSound.play();
+
+    for (Entity enemy : enemiesInRange) {
+      CombatStatsComponent enemyStats =
+          enemy.getComponent(CombatStatsComponent.class);
+
+      if (enemyStats != null) {
+        enemyStats.hit(combatStats);
+      }
+    }
+  }
+
+  private void onCollisionStart(Fixture me, Fixture other) {
+    if (hitboxComponent.getFixture() != me) {
+      return;
+    }
+
+    if (!PhysicsLayer.contains(
+        PhysicsLayer.NPC, other.getFilterData().categoryBits)) {
+      return;
+    }
+
+    BodyUserData userData =
+        (BodyUserData) other.getBody().getUserData();
+
+    if (userData != null && userData.entity != null) {
+      enemiesInRange.add(userData.entity);
+    }
+  }
+
+  private void onCollisionEnd(Fixture me, Fixture other) {
+    if (hitboxComponent.getFixture() != me) {
+      return;
+    }
+
+    if (!PhysicsLayer.contains(
+        PhysicsLayer.NPC, other.getFilterData().categoryBits)) {
+      return;
+    }
+
+    BodyUserData userData =
+        (BodyUserData) other.getBody().getUserData();
+
+    if (userData != null && userData.entity != null) {
+      enemiesInRange.remove(userData.entity);
+    }
   }
 }
