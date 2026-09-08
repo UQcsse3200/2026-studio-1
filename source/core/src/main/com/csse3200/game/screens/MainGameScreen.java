cbaf031c304d0d28c9f9fa59335a3d664da9b836
Rashim <your.email@example.com>
2026-09-08 16:53:10 +1000

    Added win screen

--- source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
@@ -9,6 +9,7 @@ import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.components.gamearea.PerformanceDisplay;
 import com.csse3200.game.components.maingame.DeathScreenDisplay;
 import com.csse3200.game.components.maingame.MainGameActions;
+import com.csse3200.game.components.maingame.WinScreenDisplay;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.EntityService;
 import com.csse3200.game.entities.factories.RenderFactory;
@@ -25,6 +26,7 @@ import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.ui.terminal.Terminal;
 import com.csse3200.game.ui.terminal.TerminalDisplay;
+import com.csse3200.game.ui.terminal.commands.WinCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,6 +45,7 @@ public class MainGameScreen extends ScreenAdapter {
   private final PhysicsEngine physicsEngine;
   private ForestGameArea forestGameArea;
   private DeathScreenDisplay deathScreenDisplay;
+  private WinScreenDisplay winScreenDisplay;
   private boolean deathScreenShown = false;
 
   public MainGameScreen(GdxGame game) {
@@ -152,9 +155,13 @@ public class MainGameScreen extends ScreenAdapter {
 
     Entity ui = new Entity();
     deathScreenDisplay = new DeathScreenDisplay(this.game);
+    winScreenDisplay = new WinScreenDisplay(this.game);
+
+    Terminal terminal = new Terminal();
+    terminal.addCommand("win", new WinCommand(winScreenDisplay));
     ui.addComponent(new InputDecorator(stage, 10))
         .addComponent(new PerformanceDisplay())
-        .addComponent(new Terminal())
+        .addComponent(terminal)
         .addComponent(inputComponent)
         .addComponent(new TerminalDisplay())
         .addComponent(new PauseMenuComponent())
@@ -162,6 +169,7 @@ public class MainGameScreen extends ScreenAdapter {
         .addComponent(new PauseMenuDisplay())
         .addComponent(new PauseMenuInputComponent())
         .addComponent(deathScreenDisplay)
+        .addComponent(winScreenDisplay)
         .addComponent(new MainGameActions(this.game))
         .addComponent(new PauseMenuActions());
 
