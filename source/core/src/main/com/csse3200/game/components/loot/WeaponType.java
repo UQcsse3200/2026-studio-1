6dfa4ae43dad77ae3cf9f11774d4ffa40e28cb4e
PadawanTano733 <sarah@slashtech.com.au>
2026-09-12 19:24:53 +1000

    Revert "refactored weapon logic so weapon item does tier logic not weapon generator."
    This reverts commit fcb6854bdde306b037d7ccc9ccb19022412cfaac.

--- source/core/src/main/com/csse3200/game/components/loot/WeaponType.java
+++ source/core/src/main/com/csse3200/game/components/loot/WeaponType.java
@@ -3,7 +3,26 @@ package com.csse3200.game.components.loot;
 // Represents the different types of weapons available in the game.
 
 public enum WeaponType {
-  SWORD,
-  BOW,
-  DAGGER
+  SWORD(/* baseDamage */ 10),
+  BOW( /* baseDamage */ 7),
+  DAGGER(/* baseDamage */ 3);
+
+  private final int baseDamage;
+
+  /**
+   * Constructor to set Base damage for each weapon type
+   * @param baseDamage - tier 1 damage made by weapon
+   */
+  WeaponType(int baseDamage) {
+    this.baseDamage=baseDamage;
+  }
+
+  /**
+   * Returns the actual base damage set for each weapon type
+   * @return this type's tier-1 damage
+   */
+  public int getBaseDamage() {
+    return this.baseDamage;
+  }
+
 }
