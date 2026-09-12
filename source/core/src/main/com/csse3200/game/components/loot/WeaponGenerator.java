3dd534fd70ecec61d68473766c8df8dbdf84cb63
PadawanTano733 <sarah@slashtech.com.au>
2026-09-12 19:31:31 +1000

    refactored weapon logic so weapon item does tier logic not weapon generator.

--- source/core/src/main/com/csse3200/game/components/loot/WeaponGenerator.java
+++ source/core/src/main/com/csse3200/game/components/loot/WeaponGenerator.java
@@ -2,29 +2,25 @@ package com.csse3200.game.components.loot;
 
 // Generates weapon items with properties based on weapon type and loot tier.
 
+import static com.csse3200.game.components.loot.WeaponType.*;
+
 public class WeaponGenerator {
 
-  public WeaponItem generateWeapon(WeaponType weaponType, int tier) {
+  public WeaponItem generateWeapon(WeaponType weaponType, int tier) throws IllegalArgumentException {
     if (weaponType == null) {
       throw new IllegalArgumentException("WeaponType must not be null.");
     }
 
-    if (tier <= 0) {
-      throw new IllegalArgumentException("Tier must be greater than 0.");
-    }
+//    if (tier <= 0) {
+//      throw new IllegalArgumentException("Tier must be greater than 0.");
+//    }
+    String name = switch(weaponType) {
+      case SWORD -> "Basic Sword";
+      case BOW -> "Basic Bow";
+      case DAGGER -> "Basic Dagger";
+      default -> throw new IllegalArgumentException("Unsupported weapon type.");
+    };
 
-    switch (weaponType) {
-      case SWORD:
-        return new WeaponItem("Basic Sword", WeaponType.SWORD, 10 * tier, 1, 1);
-
-      case BOW:
-        return new WeaponItem("Basic Bow", WeaponType.BOW, 7 * tier, 1, 1);
-
-      case DAGGER:
-        return new WeaponItem("Basic Dagger", WeaponType.DAGGER, 3 * tier, 1, 1);
-
-      default:
-        throw new IllegalArgumentException("Unsupported weapon type.");
-    }
+    return new WeaponItem(name, weaponType, tier, 1, 1);
   }
 }
--- source/core/src/main/com/csse3200/game/components/loot/WeaponType.java
+++ source/core/src/main/com/csse3200/game/components/loot/WeaponType.java
@@ -4,25 +4,26 @@ package com.csse3200.game.components.loot;
 
 public enum WeaponType {
   SWORD(/* baseDamage */ 10),
-  BOW( /* baseDamage */ 7),
+  BOW(/* baseDamage */ 7),
   DAGGER(/* baseDamage */ 3);
 
   private final int baseDamage;
 
   /**
    * Constructor to set Base damage for each weapon type
+   *
    * @param baseDamage - tier 1 damage made by weapon
    */
   WeaponType(int baseDamage) {
-    this.baseDamage=baseDamage;
+    this.baseDamage = baseDamage;
   }
 
   /**
    * Returns the actual base damage set for each weapon type
+   *
    * @return this type's tier-1 damage
    */
   public int getBaseDamage() {
     return this.baseDamage;
   }
-
 }
