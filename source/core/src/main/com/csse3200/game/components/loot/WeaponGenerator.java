20caf204ba046d7e3d819ef0621ac122cf0e4341
PadawanTano733 <sarah@slashtech.com.au>
2026-09-12 19:37:28 +1000

    spotlessApply run.

--- source/core/src/main/com/csse3200/game/components/loot/WeaponGenerator.java
+++ source/core/src/main/com/csse3200/game/components/loot/WeaponGenerator.java
@@ -6,20 +6,22 @@ import static com.csse3200.game.components.loot.WeaponType.*;
 
 public class WeaponGenerator {
 
-  public WeaponItem generateWeapon(WeaponType weaponType, int tier) throws IllegalArgumentException {
+  public WeaponItem generateWeapon(WeaponType weaponType, int tier)
+      throws IllegalArgumentException {
     if (weaponType == null) {
       throw new IllegalArgumentException("WeaponType must not be null.");
     }
 
-//    if (tier <= 0) {
-//      throw new IllegalArgumentException("Tier must be greater than 0.");
-//    }
-    String name = switch(weaponType) {
-      case SWORD -> "Basic Sword";
-      case BOW -> "Basic Bow";
-      case DAGGER -> "Basic Dagger";
-      default -> throw new IllegalArgumentException("Unsupported weapon type.");
-    };
+    //    if (tier <= 0) {
+    //      throw new IllegalArgumentException("Tier must be greater than 0.");
+    //    }
+    String name =
+        switch (weaponType) {
+          case SWORD -> "Basic Sword";
+          case BOW -> "Basic Bow";
+          case DAGGER -> "Basic Dagger";
+          default -> throw new IllegalArgumentException("Unsupported weapon type.");
+        };
 
     return new WeaponItem(name, weaponType, tier, 1, 1);
   }
--- source/core/src/main/com/csse3200/game/components/loot/WeaponItem.java
+++ source/core/src/main/com/csse3200/game/components/loot/WeaponItem.java
@@ -1,12 +1,10 @@
 package com.csse3200.game.components.loot;
 
 /**
- * Represents a weapon item that can be stored in any entity's inventory and usable to supply
- * an attack component's damage. Damage is computed from type + tier on demand.
- * A weapon has the common properties of an Item, as well as a weapon type
- * and damage value.
+ * Represents a weapon item that can be stored in any entity's inventory and usable to supply an
+ * attack component's damage. Damage is computed from type + tier on demand. A weapon has the common
+ * properties of an Item, as well as a weapon type and damage value.
  */
-
 public class WeaponItem extends Item {
 
   private final WeaponType weaponType;
@@ -14,7 +12,7 @@ public class WeaponItem extends Item {
 
   // Creates a weapon item.
   public WeaponItem(String name, WeaponType weaponType, int tier, int quantity, int maxQuantity)
-  throws IllegalArgumentException {
+      throws IllegalArgumentException {
 
     super(name, ItemType.WEAPON, quantity, maxQuantity);
 
