4a627375bc69551af2c8f525945485a260b4c097
PadawanTano733 <sarah@slashtech.com.au>
2026-09-12 18:49:53 +1000

    refactored weapon logic so weapon item does tier logic not weapon generator.

--- source/core/src/main/com/csse3200/game/components/loot/WeaponType.java
+++ source/core/src/main/com/csse3200/game/components/loot/WeaponType.java
@@ -2,6 +2,13 @@ package com.csse3200.game.components.loot;
 
 // Represents the different types of weapons available in the game.
 
+/**
+ * The set of weapon stypes avaliable in the game. Each constant carries only a hardcoded base
+ * damage - range and knockback are NOT weapon properties - the enemy types will have different
+ * ranges and strength of knockback depending on their size i.e. minotaur vs skeleton.
+ * Reach/force depend on the weilder's body and animation, not the weapon). Damage scales
+ * with tier - see {@link WeaponItem#getDamage()}.
+ */
 public enum WeaponType {
   SWORD(/* baseDamage */ 10),
   BOW( /* baseDamage */ 7),
