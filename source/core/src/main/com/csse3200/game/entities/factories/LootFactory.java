dcc9882f16a4965bdd90bda30fc649728f3bb54a
PadawanTano733 <sarah@slashtech.com.au>
2026-09-15 14:15:19 +1000

    spotlessApply ran

--- source/core/src/main/com/csse3200/game/entities/factories/LootFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/LootFactory.java
@@ -1,6 +1,5 @@
 package com.csse3200.game.entities.factories;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.csse3200.game.components.loot.ConsumableItem;
@@ -17,6 +16,7 @@ import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.AnimationRenderComponent;
 import com.csse3200.game.rendering.BobbingTextureRenderComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
+import com.csse3200.game.services.ServiceLocator;
 
 /** Factory for creating loot entities that can be picked up by the player. */
 public class LootFactory {
@@ -71,7 +71,8 @@ public class LootFactory {
     } else {
       AnimationRenderComponent animator =
           new AnimationRenderComponent(
-              new TextureAtlas(Gdx.files.internal("images/items/gold_coin/gold_coin.atlas")));
+              ServiceLocator.getResourceService()
+                  .getAsset("images/items/gold_coin/gold_coin.atlas", TextureAtlas.class));
 
       animator.addAnimation("gold_coin", 0.15f, Animation.PlayMode.LOOP);
       animator.startAnimation("gold_coin");
