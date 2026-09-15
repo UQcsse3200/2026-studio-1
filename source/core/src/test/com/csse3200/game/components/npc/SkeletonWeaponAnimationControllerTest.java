2b5665cc7fc5007729d093a0521c3b947fa6db15
Connor Montiel <connormontiel@outlook.com>
2026-09-15 16:04:13 +1000

    Fixed mock tests

--- source/core/src/test/com/csse3200/game/components/npc/SkeletonWeaponAnimationControllerTest.java
+++ source/core/src/test/com/csse3200/game/components/npc/SkeletonWeaponAnimationControllerTest.java
@@ -3,6 +3,7 @@ package com.csse3200.game.components.npc;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import com.csse3200.game.components.ComponentPriority;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.rendering.EnemyWeaponAnimationComponent;
@@ -24,7 +25,8 @@ class SkeletonWeaponAnimationControllerTest {
   void beforeEach() {
     entity = new Entity();
     controller = new SkeletonWeaponAnimationController();
-
+    
+    lenient().when(weaponAnimator.getPrio()).thenReturn(ComponentPriority.LOW);
     lenient().when(weaponAnimator.hasAnimation("default_r")).thenReturn(true);
     lenient().when(weaponAnimator.hasAnimation("default_l")).thenReturn(true);
     lenient().when(weaponAnimator.hasAnimation("sword_r")).thenReturn(true);
