12412466927359f12b1c8491134d85cb953b4bca
Rashim <your.email@example.com>
2026-09-21 22:48:11 +1000

    load checkpoint when starting game

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuActions.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuActions.java
@@ -29,7 +29,7 @@ public class MainMenuActions extends Component {
   /** Swaps to the Main Game screen. */
   private void onStart() {
     logger.info("Start game");
-    game.setScreen(GdxGame.ScreenType.RESTART_GAME);
+    game.setScreen(GdxGame.ScreenType.MAIN_GAME);
   }
 
   /** Intended for loading a saved game state. Load functionality is not actually implemented. */
