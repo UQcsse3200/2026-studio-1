91bfca542bc653ea9a7b7d9bace14d6644b2ae51
Rashim <your.email@example.com>
2026-09-21 21:55:37 +1000

    store pause menu actions component

--- source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
@@ -85,6 +85,7 @@ public class MainGameScreen extends ScreenAdapter {
   private LevelGameArea levelGameArea;
   private String currentRoomMapPath = FIRST_ROOM_MAP;
   private GameSaveData levelCheckpoint;
+  private PauseMenuActions pauseMenuActions;
   private Map<String, Long> lootSeedsByRoom = new HashMap<>();
   private DeathScreenDisplay deathScreenDisplay;
   private WinScreenDisplay winScreenDisplay;
@@ -460,6 +461,12 @@ public class MainGameScreen extends ScreenAdapter {
 
     upgradesDisplay = new UpgradesDisplay();
 
+    PauseMenuActions pauseMenuActions =
+            new PauseMenuActions(
+                    this::getPlayerEntity, this::getLootSeedsByRoom, () -> currentRoomMapPath);
+
+    this.pauseMenuActions = pauseMenuActions;
+
     ui.addComponent(new InputDecorator(stage, 10))
         .addComponent(new PerformanceDisplay())
         .addComponent(terminal)
@@ -468,9 +475,7 @@ public class MainGameScreen extends ScreenAdapter {
         .addComponent(pauseMenuComponent)
         .addComponent(new KeyboardPauseInput())
         .addComponent(new PauseMenuDisplay())
-        .addComponent(
-            new PauseMenuActions(
-                this::getPlayerEntity, this::getLootSeedsByRoom, () -> currentRoomMapPath))
+            .addComponent(pauseMenuActions)
         .addComponent(new PauseMenuInputComponent())
         .addComponent(deathScreenDisplay)
         .addComponent(new DeathScreenInputComponent())
